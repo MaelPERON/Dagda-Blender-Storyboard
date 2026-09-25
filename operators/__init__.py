c433a5861b7c892d5fb462ca3ccabb21cae93531
MaelPERON <49273112+MaelPERON@users.noreply.github.com>
2026-09-25 10:34:39 +0200

    feat(panels): create SE_PT_ThumbnailsPanel draft panel

---
+++ operators/__init__.py
@@ -0,0 +1,5 @@
+from .import_thumbnails import SE_OT_ImportStoryThumbnails
+
+__all__ = [
+    SE_OT_ImportStoryThumbnails,
+]
---
+++ panels/thumbnails.py
@@ -0,0 +1,38 @@
+import bpy
+from bl_ui.space_sequencer import SequencerButtonsPanel
+from ..operators import SE_OT_ImportStoryThumbnails
+
+
+class SE_PT_Thumbnails(SequencerButtonsPanel, bpy.types.Panel):
+    bl_label = "Storyboard Thumbnails"
+    bl_idname = "SE_PT_thumbnails_panel"
+    bl_category = "Storyboard"
+
+    def draw(self, context: bpy.types.Context):
+        layout = self.layout
+        layout.operator(
+            SE_OT_ImportStoryThumbnails.bl_idname,
+            text="Import Story Thumbnails")
+
+        scene = context.scene
+        scene: bpy.types.Scene
+        strip = scene.sequence_editor.active_strip
+
+        if not strip:
+            layout.label(text="No active strip selected.")
+            return
+
+        if strip.type != 'IMAGE':
+            layout.label(text="Active strip is not an image.")
+            return
+
+        layout.prop(strip, "name", text="Strip Name")
+
+        for prop in ["index_row", "index_col", "rows", "cols", "gutters"]:
+            layout.label(text=f"{prop}: {strip.get(prop, 'N/A')}")
+
+        width, height = strip.get("image_resolution", ("N/A", "N/A"))
+        layout.label(text=f"Image: {width} x {height}")
+
+        width, height = strip.get("thumbnail_resolution", ("N/A", "N/A"))
+        layout.label(text=f"Thumbnail: {width} x {height}")
